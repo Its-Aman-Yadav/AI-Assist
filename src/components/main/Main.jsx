54a68ec62680e7fbd75a612794f6f3712bcbacd1
Aman Yadav <itsamanyadav18@gmail.com>
2024-04-22 12:23:40 +0530

    Changed Name and Added Icon

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,131 +2,132 @@ import { useContext } from "react";
 import { assets } from "../../assets/assets";
 import "./main.css";
 import { Context } from "../../context/Context";
+
 const Main = () => {
-	const {
-		onSent,
-		recentPrompt,
-		showResults,
-		loading,
-		resultData,
-		setInput,
-		input,
-	} = useContext(Context);
+    const {
+        onSent,
+        recentPrompt,
+        showResults,
+        loading,
+        resultData,
+        setInput,
+        input,
+    } = useContext(Context);
 
     const handleCardClick = (promptText) => {
-			setInput(promptText);
-		};
-	return (
-		<div className="main">
-			<div className="nav">
-				<p>Gemini</p>
-				<img src={assets.user} alt="" />
-			</div>
-			<div className="main-container">
-				{!showResults ? (
-					<>
-						<div className="greet">
-							<p>
-								<span>Hello , Dev </span>
-							</p>
-							<p>How Can i Help You Today?</p>
-						</div>
-						<div className="cards">
-							<div
-								className="card"
-								onClick={() =>
-									handleCardClick("Suggest Some Place To Visit In Kerala")
-								}
-							>
-								<p>Suggest Some Place To Visit In Kerala </p>
-								<img src={assets.compass_icon} alt="" />
-							</div>
-							<div
-								className="card"
-								onClick={() =>
-									handleCardClick(
-										"Brainstorm team bonding activities for our work retreat"
-									)
-								}
-							>
-								<p>Brainstorm team bonding activities for our work retreat </p>
-								<img src={assets.message_icon} alt="" />
-							</div>
-							<div
-								className="card"
-								onClick={() =>
-									handleCardClick("How to Create a Gyroscope using Disc?")
-								}
-							>
-								<p>How to Create a Gyroscope using Disc?</p>
-								<img src={assets.bulb_icon} alt="" />
-							</div>
-							<div
-								className="card"
-								onClick={() => {
-									handleCardClick(
-										"Create a Script for the youtube video about coding "
-									);
-								}}
-							>
-								<p>Create a Script for the youtube video about coding </p>
-								<img src={assets.code_icon} alt="" />
-							</div>
-						</div>
-					</>
-				) : (
-					<div className="result">
-						<div className="result-title">
-							<img src={assets.user} alt="" />
-							<p>{recentPrompt}</p>
-						</div>
-						<div className="result-data">
-							<img src={assets.gemini_icon} alt="" />
-							{loading ? (
-								<div className="loader">
-									<hr />
-									<hr />
-									<hr />
-								</div>
-							) : (
-								<p dangerouslySetInnerHTML={{ __html: resultData }}></p>
-							)}
-						</div>
-					</div>
-				)}
+        setInput(promptText);
+    };
 
-				<div className="main-bottom">
-					<div className="search-box">
-						<input
-							onChange={(e) => {
-								setInput(e.target.value);
-							}}
-							value={input}
-							type="text"
-							placeholder="Enter the Prompt Here"
-						/>
-						<div>
-							<img src={assets.gallery_icon} alt="" />
-							<img src={assets.mic_icon} alt="" />
-							<img
-								src={assets.send_icon}
-								alt=""
-								onClick={() => {
-									onSent();
-								}}
-							/>
-						</div>
-					</div>
-					<div className="bottom-info">
-						<p>
-							Gemini may display inaccurate info, including about people, so
-							double-check its responses. Your privacy & Gemini Apps
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSent();
+    };
+
+    return (
+        <div className="main">
+            <div className="nav">
+                <p>AI Assist</p>
+                <img src={assets.user} alt="" />
+            </div>
+            <div className="main-container">
+                {!showResults ? (
+                    <>
+                        <div className="greet">
+                            <p>
+                                <span>Hi Sir, </span>
+                            </p>
+                            <p>How Can i Help You Today?</p>
+                        </div>
+                        <div className="cards">
+                            <div
+                                className="card"
+                                onClick={() =>
+                                    handleCardClick(
+                                        "Suggest Some Place To visit in Noida"
+                                    )
+                                }
+                            >
+                                <p>Suggest Some Place To visit in Noida</p>
+                                <img src={assets.compass_icon} alt="" />
+                            </div>
+                            <div
+                                className="card"
+                                onClick={() =>
+                                    handleCardClick("What to do in my free time?")
+                                }
+                            >
+                                <p>What to do in my free time? </p>
+                                <img src={assets.message_icon} alt="" />
+                            </div>
+                            <div
+                                className="card"
+                                onClick={() =>
+                                    handleCardClick("How to make paneer tikka masala")
+                                }
+                            >
+                                <p>How to make paneer tikka masala</p>
+                                <img src={assets.bulb_icon} alt="" />
+                            </div>
+                            <div
+                                className="card"
+                                onClick={() => {
+                                    handleCardClick(
+                                        "Create a Simple landing page for a youtuber "
+                                    );
+                                }}
+                            >
+                                <p>Create a Simple landing landing page for a youtuber</p>
+                                <img src={assets.code_icon} alt="" />
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    <div className="result">
+                        <div className="result-title">
+                            <img src={assets.user} alt="" />
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className="result-data">
+                            <img src={assets.gemini_icon} alt="" />
+                            {loading ? (
+                                <div className="loader">
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                                </div>
+                            ) : (
+                                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                            )}
+                        </div>
+                    </div>
+                )}
+
+                <div className="main-bottom">
+                    <form onSubmit={handleSubmit}>
+                        <div className="search-box">
+                            <input
+                                onChange={(e) => {
+                                    setInput(e.target.value);
+                                }}
+                                value={input}
+                                type="text"
+                                placeholder="Enter the Prompt Here"
+                            />
+                            <div>
+                               
+                                <button type="submit">
+                                    <img src={assets.send_icon} alt="" />
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    <div className="bottom-info">
+                        <p>Info may be false, use with caution</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Main;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -43,20 +43,7 @@ const Sidebar = () => {
 					</div>
 				) : null}
 			</div>
-			<div className="bottom">
-				<div className="bottom-item recent-entry">
-					<img src={assets.question_icon} alt="" />
-					{extended ? <p>Help</p> : null}
-				</div>
-				<div className="bottom-item recent-entry">
-					<img src={assets.history_icon} alt="" />
-					{extended ? <p>Activity</p> : null}
-				</div>
-				<div className="bottom-item recent-entry">
-					<img src={assets.setting_icon} alt="" />
-					{extended ? <p>Settings</p> : null}
-				</div>
-			</div>
+			
 		</div>
 	);
 };
